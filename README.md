e59439aca89ab6f0d1ffcef5858687fa01d4c6af
Eril Shaji <eril@192.168.1.5>
2026-06-26 11:36:20 +0530

    chore: initialize project structure

---
+++ README.md
@@ -0,0 +1,19 @@
+# Restaurant KDS
+
+A Kitchen Display System (KDS) for restaurants.
+
+## Getting Started
+
+TBD
+
+## Installation
+
+TBD
+
+## Usage
+
+TBD
+
+## Contributing
+
+TBD
---
+++ backend/src/main/java/com/eril/backend/BackednApplication.java
@@ -0,0 +1,13 @@
+package com.eril.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackednApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackednApplication.class, args);
+	}
+
+}
